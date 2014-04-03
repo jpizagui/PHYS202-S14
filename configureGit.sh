ad39ddd22c85200eef3fe9b9a9d36daf3bb4f206
Joseph Izaguirre <jpizagui@calpoly.edu>
2014-04-03 12:54:29 -0700

    adding config and pledge

---
+++ configureGit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "Joseph Izaguirre"
+git config --global user.email "jpizagui@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
