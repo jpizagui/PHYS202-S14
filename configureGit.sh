dc8029a2dd56d1fe51004c470cd8717d49f136e6
Joseph Izaguirre <jpizagui@calpoly.edu>
2014-04-03 13:05:10 -0700

    added documentation

--- configureGit.sh
+++ configureGit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script configures git whenever you start working
+# on multiple machines. Run it any time you decide to work
+# with git on a new computer.
 git config --global user.name "Joseph Izaguirre"
 git config --global user.email "jpizagui@calpoly.edu"
 git config --global color.ui "auto"
